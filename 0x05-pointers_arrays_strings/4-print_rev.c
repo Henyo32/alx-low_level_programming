f3846e18b12f7982fae59a7b81b72dfcb17d108e
Henyo32 <henyofirst29@gmail.com>
2022-06-13 03:42:36 -0700

    my

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 /**
  * print_rev - a function that prints a string, in reverse
  * fcounter is to first count to end, n is to count back
@@ -19,3 +18,16 @@ for (n = (fcounter - 1); n >= 0; n--)
 {
 }
 }
+int main(void)
+{
+char *s;
+
+s = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+print_rev(s);
+return (0);
+}
+
+
+
+
+
