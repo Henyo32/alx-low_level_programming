8322167df6d9790cb71d81a137bd795036ca67a7
Henyo32 <henyofirst29@gmail.com>
2022-06-24 12:25:17 -0700

    mine

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * main -entry point
- * Description: - 'program description'
- * return: always 0
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0
  */
 int main(void)
 {
-_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+_puts("\"Attheendoftheday,mygoalwasto be thebest hacker\"\n\t- Kevin Mitnick");
 return (0);
 }
